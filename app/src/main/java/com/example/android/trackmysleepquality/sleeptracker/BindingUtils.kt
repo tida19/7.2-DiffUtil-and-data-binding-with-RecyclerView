065a65325a6f5b24032da846cef8b4a8e65a4148
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2020-09-05 17:51:15 +0700

    7.2-7-Step 3: Add bindings to XML layout

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -5,6 +5,7 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 
 @BindingAdapter("sleepDurationFormatted")
@@ -23,4 +24,8 @@ fun ImageView.setSleepImage(item: SleepNight) {
         5 -> R.drawable.ic_sleep_5
         else -> R.drawable.ic_sleep_active
     })
+}
+@BindingAdapter("sleepQualityString")
+fun TextView.setSleepQualityString(item: SleepNight) {
+    text = convertNumericQualityToString(item.sleepQuality, context.resources)
 }
